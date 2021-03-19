56fd4d699cbe700353fdcc0e5d3996952273aad9
Jueun Seo <zuneseo@buzzvil.com>
2021-03-19 18:42:15 +0900

    중복된 파일 발견시 다른 이름 사용

--- text_cleaner_main.py
+++ text_cleaner_main.py
@@ -16,7 +16,7 @@ from qasync import QEventLoop, asyncSlot
 from text_file_cleaner import TextFileCleaner
 from text_filters import UUIDDashFilter, UUIDSearchFilter
 
-APP_VERSION = '0.1.2'
+APP_VERSION = '0.1.3'
 DEBUG = False
 
 
--- text_file_cleaner.py
+++ text_file_cleaner.py
@@ -51,10 +51,27 @@ class TextFileCleaner():
     def get_num_excluded(self):
         return self._num_excluded
 
+    def get_suffixed_filename(self, path, index):
+        filename, file_extension = os.path.splitext(path)
+        if file_extension:
+            return '{}({}){}'.format(filename, index, file_extension)
+        else:
+            return '{}({}})'.format(filename, index)
+
+    def get_none_duplicated_path(self, path):
+        if not os.path.exists(path):
+            return path
+        for i in range(1, 100):
+            new_path = self.get_suffixed_filename(path, i)
+            if not os.path.exists(new_path):
+                return new_path
+        raise Exception("Too many duplicated files")
+
     def process(self):
         for path in self.path_list:
             with csv_excel_reader(path) as in_file:
                 out_filename = self.add_prefix(path, 'cleaned_').replace('.xlsx', '.csv').replace('.xls', '.csv')
+                out_filename = self.get_none_duplicated_path(out_filename)
                 with open(out_filename, 'w', encoding='utf-8') as out_file:
                     for processed_lines in self.process_file(in_file, out_file):
                         yield processed_lines
